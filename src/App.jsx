a5b465cabd9fa20c00ed19a1f78ce12fde2da325
aismeg <iaskja20@gmai.com>
2025-06-06 15:49:03 +0200

    beta

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import { Nav } from './Components/Nav/Nav';
 import { Sidebar } from './Components/Sidebar/Sidebar';
 import { Main } from './Components/Main/Main';
 import { useData } from './Components/Function/useData';
-import { Temp } from './Components/Temp/Temp';
 
 function App() {
   const data = useData();
 
   return (
     <div className="app-container">
-      {/* <Nav /> */}
+      <Nav />
       <div className="content-area">
-        <Temp data={data} />
-        {/* <Sidebar />
-        <Main data={data} /> */}
+        <Sidebar />
+        <Main data={data} />
       </div>
     </div>
   );
