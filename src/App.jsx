d8e3959ccdf58fa1c244fc953a6bedad5dea6c9c
aismeg <iaskja20@gmai.com>
2025-06-26 15:17:45 +0200

    section1

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import './App.css';
 import { Nav } from './Components/Nav/Nav';
 import { Sidebar } from './Components/Sidebar/Sidebar';
 import { Main } from './Components/Main/Main';
+import { Section1 } from './Components/Sidebar/Section1/Section1';
 import { useData } from './Components/Function/useData';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const data = useData();
 
   return (
-    <div className="app-container">
-      <Nav />
-      <div className="content-area">
-        <Sidebar />
-        <Main data={data} />
+    <Router>
+      <div className="app-container">
+        <Nav />
+        <div className="content-area">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Main data={data} />} />
+            <Route path="/section1" element={<Section1 />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
